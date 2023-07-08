8bf7f5bc22fbec17b6a388308f333d14da53010b
Van Hohenheim <0xhohenheim@gmail.com>
2023-07-08 19:52:50 +0530

    fix: contact alignment

--- components/home/contactUs.tsx
+++ components/home/contactUs.tsx
@@ -5,33 +5,35 @@ import { DocsIcon, TelegramIcon, TwitterIcon } from "../icons";
 export default function ContactUs(): JSX.Element {
   return (
     <div className={styles.contact_us}>
-      <Link className={styles.contact_card} href="https://web.telegram.org/">
-        <span className={styles.icon}>
-          <TelegramIcon />
-        </span>
-        <span className={styles.handles}>
-          <p>JOIN OUR</p>
-          <h3>Telegram</h3>
-        </span>
-      </Link>
-      <Link className={styles.contact_card} href="https://twitter.com/">
-        <span className={styles.icon}>
-          <TwitterIcon />
-        </span>
-        <span className={styles.handles}>
-          <p>FOLLOW OUR</p>
-          <h3>Twitter</h3>
-        </span>
-      </Link>
-      <Link className={styles.contact_card} href="https://google.com">
-        <span className={styles.icon}>
-          <DocsIcon />
-        </span>
-        <span className={styles.handles}>
-          <p>SEE OUR</p>
-          <h3>LitePaper</h3>
-        </span>
-      </Link>
+      <div className={styles.innerContainer}>
+        <Link className={styles.contact_card} href="https://web.telegram.org/">
+          <span className={styles.icon}>
+            <TelegramIcon />
+          </span>
+          <span className={styles.handles}>
+            <p>JOIN OUR</p>
+            <h3>Telegram</h3>
+          </span>
+        </Link>
+        <Link className={styles.contact_card} href="https://twitter.com/">
+          <span className={styles.icon}>
+            <TwitterIcon />
+          </span>
+          <span className={styles.handles}>
+            <p>FOLLOW OUR</p>
+            <h3>Twitter</h3>
+          </span>
+        </Link>
+        <Link className={styles.contact_card} href="https://google.com">
+          <span className={styles.icon}>
+            <DocsIcon />
+          </span>
+          <span className={styles.handles}>
+            <p>SEE OUR</p>
+            <h3>LitePaper</h3>
+          </span>
+        </Link>
+      </div>
     </div>
   );
 }
