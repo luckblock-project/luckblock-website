533e525744d0f7dc70a04f8fb6b8081ce2eff64a
AbhishekJadhav2002 <www.abhishek3jadhav@gmail.com>
2023-07-10 13:53:45 +0530

    chore: separate component for hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,16 @@
-import Badge from "@/components/badge";
 import ContactUs from "@/components/home/contactUs";
+import Hero from "@/components/home/hero";
 import { OurToken } from "@/components/home/ourToken";
 import Roadmap from "@/components/home/roadmap";
 import { Solutions } from "@/components/home/solutions";
-import { AngleRightIcon, PlayIcon } from "@/components/icons";
 import { SectionHead } from "@/components/sectionHead";
-import { UnderlineText } from "@/components/underlineText";
 import styles from "@/styles/page.module.scss";
-import Link from "next/link";
-import Image from "next/image";
-import HeroRobo from "../public/hero-robo.png"
 
 export default function Home(): JSX.Element {
   return (
     <main className={styles.home_page}>
       {/* HERO */}
-      <section id="home" className={styles.hero}>
-        <div className={styles.hero_content}>
-          <Badge className={styles.hero_badge}>
-            <Image src="./pie.svg" alt="pie icon" height={20} width={20} />
-            New Version Launched
-          </Badge>
-          <h1 title="$ROVER: Blockchain Innovation">
-            Introducing{" "}
-            <UnderlineText
-              text="$ROVER"
-              className={styles.hero_content_underline_text}
-            />{" "}
-            Unleashing the Power of Blockchain Innovation
-          </h1>
-          <p>
-            Unlock the limitless potential of the blockchain with $ROVER, the
-            trailblazing token revolutionizing the digital landscape. Pioneering
-            an era of transparency, security, and efficiency
-          </p>
-          <div className={styles.hero_buttons}>
-            <Link className={styles.hero_btn} href="/">
-              BETA <AngleRightIcon />
-            </Link>
-          </div>
-        </div>
-        <div className={styles.hero_image}>
-          <Image src={HeroRobo} alt="BlockRover Bot" />
-          <div className={styles.image_glow} />
-        </div>
-      </section>
+      <Hero />
       {/* OUR SOLUTIONS */}
       <section id="solutions">
         <SectionHead
